224a57b3a295cd51d091e9b3693a7e02c3d074fc
unknown <somaansh.virmani@gmail.com>
2025-04-20 01:38:41 +0530

    fixed cors

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -131,7 +131,7 @@ app.get('/auth/google/callback', passport.authenticate('google', { failureRedire
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 2 * 60 * 60 * 1000,
     path: '/',
   });
